f5032f98ffd91ecc2c95ec1484359bc9832ab4d7
Stanley <stanleyrylan3@gmail.com>
2026-02-14 15:55:40 +0000

    full project base

---
+++ code/script.js
@@ -0,0 +1,50 @@
+const envelope = document.getElementById("envelope-container");
+const letter = document.getElementById("letter-container");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".yes-btn");
+
+const title = document.getElementById("letter-title");
+const catImg = document.getElementById("letter-cat");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.getElementById("final-text");
+
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout(() => {
+        document.querySelector(".letter-window").classList.add("open")    
+    },50);
+});
+
+
+let yesScale = 1;
+
+yesBtn.style.position = "relative"
+yesBtn.style.transformOrigin = "center center"
+yesBtn.style.transition = "transform 0.3s ease"
+
+noBtn.addEventListener("click", () => {
+    yesScale += 2;
+
+    if (yesBtn.style.position !== "fixed") {
+        yesBtn.style.position = "fixed";
+        yesBtn.style.top   = "50%";
+        yesBtn.style.left = "50%";
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    }else{
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    }
+});
+
+yesBtn.addEventListener("click", () => {
+    title.textContent = "Yippeeeee!";
+
+    catImg.src = "cat_dance.gif";
+
+    document.querySelector(".letter-window").classList.add("final");
+
+    buttons.style.display = "none";
+
+    finalText.style.display = "block";
+});
